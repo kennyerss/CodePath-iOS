28493656a5beaf0605461f08f61f814a503cd8fc
Kent Canonigo <kbcanonigo@gmail.com>
2022-07-13 17:00:14 -0500

    Finalized app features colored UI interface

---
+++ SettingsViewController.swift
@@ -0,0 +1,72 @@
+//
+//  SettingsViewController.swift
+//  iOS-prework
+//
+//  Created by Kent Brylle Canonigo on 7/11/22.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    // Status for alert
+    
+
+    @IBOutlet weak var newMinimumTip: UITextField!
+    @IBOutlet weak var newMaximumTip: UITextField!
+    @IBOutlet weak var newRange: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Settings"
+        
+    }
+    
+    var showingAlert = false
+    
+    @IBAction func applyChanges(_ sender: Any) {
+        
+        // Get instance of main controller
+        let controller = storyboard?.instantiateViewController(withIdentifier: "mainVC") as! ViewController
+
+        // Change controller property to new text field in SettingsViewController
+        
+        // Changing the min and max properties
+        let newMin = Double(newMinimumTip.text!) ?? 0
+        let newMax = Double(newMaximumTip.text!) ?? 0
+        
+        // New range of the min and max values
+        let difference: Double = newMax - newMin
+        
+        let range = Double(newRange.text!) ?? 0
+        controller.minTip = newMin
+        controller.maxTip = newMax
+        
+        // Show alert if unequal range between the difference of intervals and range
+        if (Int(difference) % Int(range)) != 0 {
+            showingAlert = true
+            if showingAlert != false{
+                let alert = UIAlertController(title: "Error in interval range", message: "Difference between maximum and minimum tip has to be divisible by the range!", preferredStyle: .alert)
+
+                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                self.present(alert, animated:true, completion: nil)
+            }
+        }
+        
+        // Else change the step range as needed
+        controller.step = range
+        
+        present(controller, animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- iOS-prework/ViewController.swift
+++ iOS-prework/ViewController.swift
@@ -8,34 +8,168 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var currentTipPercentage: UILabel!
+    @IBOutlet weak var minTipPercentage: UILabel!
+    @IBOutlet weak var maxTipPercentage: UILabel!
+    @IBOutlet weak var tipSliderInfo: UISlider!
+    
+    @IBOutlet weak var splitAmountLabel: UILabel!
     @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var tipAmountLabel: UILabel!
-    @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
     
+    @IBOutlet weak var testingTip: UILabel!
+    
+    // Create text property from settings page
+    var minTip: Double = 0
+    var maxTip: Double = 20
+    
+//    var minimumValue: Float = minTip
+    
+//    var minTip = tipSliderInfo.minimumValue
+//    var maxTip = tipSliderInfo.maximumValue
+    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Sets title in Navigation Bar
+        self.title = "Tip Calculator"
+        
+        // Sets billAmountTextField to be first responder
+        billAmountTextField.becomeFirstResponder()
+        
+        // Sets tip slider initial min and max values
+        minTipPercentage.text = String(format: "%.0f%%", minTip)
+    
+        maxTipPercentage.text = String(format: "%.0f%%", maxTip)
+        
+        currentTipPercentage.text = minTipPercentage.text
     }
     
-    @IBAction func calculateTip(_ sender: Any) {
-        // Get the bill from text field input
+    
+    func changeMin() -> Double {
+        var currMin = Double(tipSliderInfo.minimumValue)
+        currMin = minTip
+        return currMin
+    }
+    
+    func changeMax() -> Double {
+        var currMax = Double(tipSliderInfo.maximumValue)
+        currMax = maxTip
+        return currMax
+    }
+    
+    
+    // Create a step range for tip slider
+    var step: Double = 5
+    
+    // Create variable for splitAmount as a Double
+    var splitAmount: Double = 0
+    
+    // Create variable for percentageValueAmount
+    var percentageValueAmount: Double = 0
+    
+    @IBAction func tipSliderPercentage(_ sender: UISlider) {
+        tipSliderInfo.maximumValue = Float(changeMax())
+        tipSliderInfo.minimumValue = Float(changeMin())
+//        tipSliderInfo.minimumValue: Double = minTip
+//        tipSliderInfo.maximumValue: Double = maxTip
+        
+        var percentageValue: Double = Double(sender.value)
+        
+        // Update label when slider is sliding
+        let roundedValue: Double = round (percentageValue / step) * step
+        
+        // Make percentageValue to roundedValue
+        percentageValue = roundedValue
+        
+        // Update currentTipPercentage label
+        currentTipPercentage.text = String(format:"%.0f%%", percentageValue)
+        
+        percentageValueAmount = percentageValue
+        
+        // Update testing-tip label
+        testingTip.text = String(percentageValueAmount)
+        
         let bill = Double(billAmountTextField.text!) ?? 0
         
-        // Get total tip by multiplying tip * percentage
-        let tipPercentage = [0.15, 0.18, 0.20]
-        let tip = bill * tipPercentage[tipControl.selectedSegmentIndex]
-        
+        let percentage = percentageValueAmount / 100
+        let tip = bill * percentage
         let total = bill + tip
         
-        // Update tip amount label
+        if splitAmount == 0 {
+            // Update total amount
+            totalLabel.text = String(format: "$%.2f", total)
+        }
+
+        // Update total amount
+        totalLabel.text = String(format: "$%.2f", total / (splitAmount + 1))
+        
+        // Update tip text
         tipAmountLabel.text = String(format: "$%.2f", tip)
         
-        // Update total amount
-        totalLabel.text = String(format: "$%.2f", total)
     }
     
+    @IBAction func decreaseSplitAmount(_ sender: UIButton) {
+        
+        // If splitAmount is already 0, then nothing shoud happen
+        
+        if splitAmount == 0 {
+            splitAmount = 0
+        }
+    
+        else {
+            // Otherwise decrement splitAmount variable
+            splitAmount -= 1
+        }
+    
+        // Update splitAmount label
+        splitAmountLabel.text = String(format: "%.0f", splitAmount)
+        
+        // Get bill from billAmountTextField
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        // Get the total tip amount
+        let percentage = percentageValueAmount / 100
+        let tip = bill * percentage
+        let total = bill + tip
+    
+        if splitAmount == 0 {
+            // Update total amount
+            totalLabel.text = String(format: "$%.2f", total)
+        }
+        
+        // Update total amount
+        totalLabel.text = String(format: "$%.2f", total / (splitAmount + 1))
+    }
+    
+    @IBAction func increaseSplitAmount(_ sender: UIButton) {
+        
+        // Increase the amount of people to split bill with
+       splitAmount += 1
+        
+        // Update split amount label
+        splitAmountLabel.text = String(format: "%.0f", splitAmount)
+        
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        let percentage = percentageValueAmount / 100
+        let tip = bill * percentage
+        let total = bill + tip
+        
+        if splitAmount == 0 {
+            // Update total amount
+            totalLabel.text = String(format: "$%.2f", total)
+        }
 
+        // Update total amount
+        totalLabel.text = String(format: "$%.2f", total / (splitAmount + 1))
+        
+
+    }
+    
+    
 }
 
